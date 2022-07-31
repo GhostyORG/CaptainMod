75ea810ae5ff8c92bc7a202005abc3868a0cd3cb
Commander R <wmxavier2004@gmail.com>
2022-07-31 14:26:16 +0200

    Create guildDelete.js

---
+++ events/guildDelete.js
@@ -0,0 +1,23 @@
+module.exports = {
+	name: 'guildDelete',
+	execute(guild) {
+		// gets the bots logging channel
+		const b_logs = client.channels.cache.get(config.channels.logs_chan);
+        
+		// creates the embed for the log channel
+        const embed = new discord.EmbedBuilder()
+        	.setColor(`${config.colors.red}`)
+			.setTitle(`Guild joined: ${guild.name}`)
+			.setThumbnail(guild.iconURL({dynamic: true}))
+			.addFields(
+				{ name: 'Guild ID', value: guild.id, inline: true },
+				{ name: 'Owner ID', value: guild.ownerId, inline: true },
+				{ name: `Members`, value: `${guild.memberCount}`, inline: true},
+				{ name: `Created At`, value: `${guild.createdAt}`, inline: true},
+			)
+			.setTimestamp()
+
+		// sends the embed to the log channel
+		b_logs.send({embeds: [embed]})
+	},
+};
