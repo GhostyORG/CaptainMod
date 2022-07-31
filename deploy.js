ed4f4d19503b377277231682257439c290a9da24
Commander R <wmxavier2004@gmail.com>
2022-07-31 20:33:45 +0200

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -17,11 +17,11 @@ for (const folder of commandFolders) {
 const rest = new REST({ version: '9' }).setToken(token);
 
 // here it defines the commands in just one server
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then(() => console.log('Successfully registered application commands for in the test guild!'))
-	.catch(console.error);
+// rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+// 	.then(() => console.log('Successfully registered application commands for in the test guild!'))
+// 	.catch(console.error);
 
 
-// rest.put(Routes.applicationCommands(clientId), { body: commands })
-//  	.then(() => console.log('Successfully registered application commands globally!.'))
-//  	.catch(console.error);
+rest.put(Routes.applicationCommands(clientId), { body: commands })
+ 	.then(() => console.log('Successfully registered application commands globally!.'))
+ 	.catch(console.error);
